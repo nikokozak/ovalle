9fcc34a8482c2addaca947d72b4b3c32c29fbd34
Nikolai Kozak <nikokozak@gmail.com>
2022-09-07 17:58:28 -0400

    first commit

---
+++ lib/ovalle.ex
@@ -0,0 +1,18 @@
+defmodule Ovalle do
+  @moduledoc """
+  Documentation for `Ovalle`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Ovalle.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ovalle_test.exs
@@ -0,0 +1,8 @@
+defmodule OvalleTest do
+  use ExUnit.Case
+  doctest Ovalle
+
+  test "greets the world" do
+    assert Ovalle.hello() == :world
+  end
+end
