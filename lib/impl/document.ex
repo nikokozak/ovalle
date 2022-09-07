a5d19b9d27f4873d527e26e6e7a4fb1c971e6a54
Nikolai Kozak <nikokozak@gmail.com>
2022-09-07 18:36:13 -0400

    add document typespec

--- lib/impl/document.ex
+++ lib/impl/document.ex
@@ -1,2 +1,20 @@
 defmodule Ovalle.Document do
+
+  @type t :: %{
+    id: String.t,
+    abs_path: String.t,
+    rel_path: String.t,
+    type: String.t,
+    rootname: String.t,
+    original_filename: String.t,
+    hash: String.t,
+    collection: String.t,
+    added_at: DateTime.t(),
+    size: integer(),
+    meta: map()
+  }
+  
+  @callback new(filepath :: String.t) :: t()
+  @callback different?(old :: t, new :: t) :: t()
+
 end
