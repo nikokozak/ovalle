0f0fc4149424bb1f522ed30955c2b9b16e4ff001
Nikolai Kozak <nikokozak@gmail.com>
2022-09-07 19:26:09 -0400

    add utils for creation and deletion of sets, collections, as well as tests for said utils

--- lib/impl/file_utils.ex
+++ lib/impl/file_utils.ex
@@ -6,24 +6,39 @@ defmodule Ovalle.FileUtils do
 
   def base_dir, do: Application.fetch_env!(:ovalle, :archive_dir)
 
+  defp collection_path(collection_name), do: join(base_dir(), collection_name)
+  defp set_path(collection_name, set_name), do: join([base_dir(), collection_name, set_name])
+
+  defp collection_exists?(collection_name), do: File.exists?(join(base_dir(), collection_name))
+  defp set_exists?(collection_name, set_name), do: File.exists?(join([base_dir(), collection_name, set_name]))
+
   def create_collection(collection_name) do
-    target = join(base_dir(), collection_name)
-    if File.exists?(target), do: {:error, :eexist}, else: File.mkdir(target)
+    cond do
+      collection_exists?(collection_name) -> {:error, :eexist}
+      true -> File.mkdir collection_path(collection_name)
+    end
   end
 
   def delete_collection(collection_name) do
-    target = join(base_dir(), collection_name)
-    if File.exists?(target), do: File.rm_rf(target), else: :ok
+    cond do
+      collection_exists?(collection_name) -> File.rm_rf collection_path(collection_name)
+      true -> {:ok, []}
+    end
   end
 
   def create_set(collection_name, set_name) do
-    target = join([base_dir(), collection_name, set_name])
-    if File.exists?(target), do: {:error, :eexist}, else: File.mkdir(target)
+    cond do
+      not collection_exists?(collection_name) -> {:error, :no_collection}
+      set_exists?(collection_name, set_name) -> {:error, :eexist}
+      true -> File.mkdir set_path(collection_name, set_name)
+    end
   end
 
   def delete_set(collection_name, set_name) do
-    target = join([base_dir(), collection_name, set_name])
-    if File.exists?(target), do: File.rm_rf(target), else: :ok
+    cond do
+      set_exists?(collection_name, set_name) -> File.rm_rf set_path(collection_name, set_name)
+      true -> {:ok, []}
+    end
   end
 
 end
